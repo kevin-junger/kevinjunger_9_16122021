3db21f1c098f619c06485433a5d6ffacf86a8670
Kevin Junger <kevin.junger@outlook.fr>
2022-01-08 02:41:27 +0100

    🚧 wip: bills tests

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -1,15 +1,15 @@
 import { screen } from "@testing-library/dom"
-import BillsUI from "../views/BillsUI.js"
-// import Bills from "../containers/Bills.js"
 import { bills } from "../fixtures/bills.js"
-import { /*ROUTES,*/ ROUTES_PATH } from "../constants/routes.js"
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js"
 import Router from "../app/Router.js"
+import Bills from "../containers/Bills.js"
+import BillsUI from "../views/BillsUI.js"
 
 describe("Given I am connected as an employee", () => {
   window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }))
-  /*const onNavigate = (pathname) => {
+  const onNavigate = (pathname) => {
     document.body.innerHTML = ROUTES({ pathname })
-  }*/
+  }
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", () => {
       window.location.hash = ROUTES_PATH["Bills"]
@@ -26,16 +26,17 @@ describe("Given I am connected as an employee", () => {
       const datesSorted = dates.sort(antiChrono)
       expect(dates).toEqual(datesSorted)
     })
-    /*describe("When I click the eye icon on an existing bill to view the attached file", () => {
+    describe("When I click the eye icon on an existing bill to view the attached file", () => {
       test("Then the file should be opened in its own modal", () => {
         const html = BillsUI({ data: bills })
         document.body.innerHTML = html
+        const testBills = new Bills({ document, onNavigate, store: null, localStorage: window.localStorage })
         const eyeIcons = screen.getAllByTestId('icon-eye')
-        eyeIcons[0].addEventListener('click', handleClickIconEye())
+        eyeIcons[0].addEventListener('click', testBills.handleClickIconEye(eyeIcons[0]))
         userEvent.click(eyeIcons[0])
         expect(handleClickIconEye).toHaveBeenCalled()
         expect(document.getElementById('modaleFile')).toBeTruthy()
       })
-    })*/
+    })
   })
 })
