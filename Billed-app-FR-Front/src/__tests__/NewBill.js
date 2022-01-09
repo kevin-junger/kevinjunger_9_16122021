c4fdc518262dc6ee8b0b3936fc064b1ace0e792c
Kevin Junger <kevin.junger@outlook.fr>
2022-01-09 21:57:23 +0100

    :construction: wip tests

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -1,14 +1,60 @@
 import { screen } from "@testing-library/dom"
+import userEvent from "@testing-library/user-event"
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js"
+import Router from "../app/Router.js"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 
-
 describe("Given I am connected as an employee", () => {
+  window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }))
+  const onNavigate = (pathname) => {
+    document.body.innerHTML = ROUTES({ pathname })
+  }
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+    test("Then the \"New Bill\" icon shoud be highlighted", () => {
+      window.location.hash = ROUTES_PATH["Bills"]
+      document.body.innerHTML = `<div id="root"></div>`
+      Router()
+      const icon = screen.getByTestId('icon-window')
+      expect(icon.classList.contains('active-icon')).toBeTruthy()
+    })
+    test("Then a form should appear", () => {
       const html = NewBillUI()
       document.body.innerHTML = html
-      //to-do write assertion
+      const newBillForm = screen.getByTestId('form-new-bill')
+      expect(newBillForm).toBeTruthy()
+    })
+    describe("Given I am uploading a file to attach to my new bill", () => {
+      describe("When I try to upload something which is not a JPG, JPEG or PNG file (e.g. a PDF)", () => {
+        test("Then the file should be rejected", () => {
+          const html = NewBillUI()
+          document.body.innerHTML = html
+          const newBill = new NewBill({ document, onNavigate, store: null, localStorage: window.localStorage })
+          const handleChangeFile = jest.fn(newBill.handleChangeFile)
+          const inputFile = screen.getByTestId('file')
+          const file = new File(['some random data'], 'file.pdf', {type: 'application/pdf'})
+          inputFile.addEventListener('change', handleChangeFile)
+          userEvent.upload(inputFile, file)
+          expect(handleChangeFile).toBeCalled()
+          // expect(inputFile.files.length).toEqual(0)
+          expect(inputFile.value).toBe('')
+        })
+      })
+      describe("When I try to upload a JPG, JPEG or PNG file", () => {
+        test("Then the file should be accepted by the handler", () => {
+          const html = NewBillUI()
+          document.body.innerHTML = html
+          const newBill = new NewBill({ document, onNavigate, store: null, localStorage: window.localStorage })
+          const handleChangeFile = jest.fn(newBill.handleChangeFile)
+          const inputFile = screen.getByTestId('file')
+          const file = new File(['some random data'], 'file.jpg', {type: 'image/jpg'})
+          inputFile.addEventListener('change', handleChangeFile)
+          userEvent.upload(inputFile, file)
+          expect(handleChangeFile).toBeCalled()
+          // expect(inputFile.files.length).toEqual(0)
+          expect(inputFile.files.length).toEqual(1)
+        })
+      })
     })
   })
 })
